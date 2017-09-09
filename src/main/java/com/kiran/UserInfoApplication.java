731f7d2f7c8299b818aa1f6c36304b24fbd04f80
Kiran Gautam <kgautam@oceanx.com>
2017-09-09 16:29:35 -0700

    small fix

--- src/main/java/com/kiran/UserInfoApplication.java
+++ src/main/java/com/kiran/UserInfoApplication.java
@@ -12,6 +12,5 @@ public class UserInfoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext appCtx  = SpringApplication.run(UserInfoApplication.class, args);
-		ClientDataHolder.setApplicationContext(appCtx);
 	}
 }
