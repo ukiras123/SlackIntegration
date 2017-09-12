ff42850894ffbbbe19ab9ff84cabbf6353ef8253
Kiran Gautam <kgautam@oceanx.com>
2017-09-12 13:59:32 -0700

    adding global exception handler

---
+++ src/main/java/com/kiran/controller/intercept/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.kiran.controller.intercept;
+
+import com.kiran.model.response.SlackResponse;
+import com.kiran.service.exception.InvalidMove;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * @author Kiran
+ * @since 9/12/17
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({InvalidMove.class})
+    public ResponseEntity<?> handleInvalidMoveException(InvalidMove move) {
+        SlackResponse response = new SlackResponse(move.getError_message());
+        return new ResponseEntity<>(response, null, HttpStatus.OK);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<?> handleGeneralException(Exception move) {
+        SlackResponse response = new SlackResponse("Something went wrong, please contact your administrator");
+        return new ResponseEntity<>(response, null, HttpStatus.OK);
+    }
+
+
+}
