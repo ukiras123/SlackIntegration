021d11da272fe362e1656dcfe7efa1a1e2455159
Kiran Gautam <kgautam@oceanx.com>
2017-09-13 21:43:53 -0700

    adding logging capabilities

--- src/main/java/com/kiran/controller/intercept/GlobalExceptionHandler.java
+++ src/main/java/com/kiran/controller/intercept/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.kiran.controller.intercept;
 
 import com.kiran.model.response.SlackResponse;
 import com.kiran.service.exception.InvalidMove;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @ExceptionHandler({InvalidMove.class})
     public ResponseEntity<?> handleInvalidMoveException(InvalidMove move) {
         SlackResponse response = new SlackResponse(move.getError_message());
@@ -22,7 +26,8 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler({Exception.class})
-    public ResponseEntity<?> handleGeneralException(Exception move) {
+    public ResponseEntity<?> handleGeneralException(Exception ex) {
+        this.logger.error("Internal slate error. ExceptionMessage=\'{}\'. StackTrace=\'{}\'", ex.getMessage(), ex.getStackTrace());
         SlackResponse response = new SlackResponse("Something went wrong, please contact your administrator");
         return new ResponseEntity<>(response, null, HttpStatus.OK);
     }
