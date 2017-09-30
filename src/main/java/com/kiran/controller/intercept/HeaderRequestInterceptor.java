ffe448ae0ea19a044504a7c26bc475cd6c13b996
Kiran Gautam <kgautam@oceanx.com>
2017-09-29 23:12:16 -0700

    adding cors

--- src/main/java/com/kiran/controller/intercept/HeaderRequestInterceptor.java
+++ src/main/java/com/kiran/controller/intercept/HeaderRequestInterceptor.java
@@ -2,6 +2,7 @@ package com.kiran.controller.intercept;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,6 +29,26 @@ public class HeaderRequestInterceptor implements HandlerInterceptor {
     }
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // For CORS preflighted we should be expecting to receive 2 requests:
+        // 1) Browsers generated OPTIONS request for authorization with headers "origin" and "access-control-request-headers".
+        // "access-control-request-headers" should have both "x-ocx-tenant" and "x-ocx-username" inside.
+        // As on the next call we are expecting for them.
+        // 2) Normal request with "x-ocx-tenant" and "x-ocx-username" plus "origin"
+        if (!StringUtils.isBlank(request.getHeader("origin"))
+                && !StringUtils.isBlank(request.getHeader("access-control-request-headers"))
+          ) {
+            if (!request.getMethod().equals(RequestMethod.OPTIONS.name())) {
+                //someone tries to simulate CORS request. Not OPTIONS was sent.
+                String errorResponseString = "Invalid CORS request";
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                response.getWriter().write(errorResponseString);
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+
         String accept = request.getHeader("Accept");
 
         if (!accept.contains("*/*") && !StringUtils.isBlank(accept)) {
--- src/main/java/com/kiran/controller/intercept/RequestInterceptorConfig.java
+++ src/main/java/com/kiran/controller/intercept/RequestInterceptorConfig.java
@@ -2,8 +2,10 @@ package com.kiran.controller.intercept;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -18,9 +20,20 @@ public class RequestInterceptorConfig extends WebMvcConfigurerAdapter {
     @Qualifier("securityHeaderInterceptor")
     HandlerInterceptor securityHeaderInterceptor;
 
+    @Value("${endpoints.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Value("${endpoints.cors.allowed-methods}")
+    private String[] allowedMethods;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(securityHeaderInterceptor);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods(allowedMethods);
     }
 
 }
