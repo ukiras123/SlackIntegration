94fbbf0c5004e737df9aa67e2ebb5afe2787dcf5
Kiran Gautam <kgautam@oceanx.com>
2017-12-07 21:47:44 -0800

    adding date

--- src/main/java/com/kiran/service/integration/JiraAPI.java
+++ src/main/java/com/kiran/service/integration/JiraAPI.java
@@ -13,10 +13,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Kiran
@@ -65,7 +68,8 @@ public class JiraAPI {
     public String getArgoSprintDetail(boolean showAll) throws InterruptedException {
         logger.info("inside getArgoSprintDetail ---------");
         String URL = jiraUrl + "/search";
-        String payload = "{\"jql\":\"project = ARGO AND issuetype in (Bug, Story, Task) AND \\\"Epic Link\\\" = ARGO-1910 and sprint in openSprints() ORDER BY status \",\"fields\":[\"summary\",\"status\",\"assignee\",\"customfield_10008\"]}";
+        String jiraTicketUrl = "https://jira.oceanx.com/browse/";
+        String payload = "{\"jql\":\"project = ARGO AND issuetype in (Bug, Story, Task) AND \\\"Epic Link\\\" = ARGO-1910 and sprint in openSprints() ORDER BY status \",\"fields\":[\"summary\",\"status\",\"assignee\",\"customfield_10008\",\"customfield_10000\"]}";
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Basic " + getBasicAuth());
@@ -77,9 +81,15 @@ public class JiraAPI {
         JSONObject jBody = new JSONObject(strBody);
         int totalTickets = jBody.getInt("total");
         int doneTickets = 0;
+        String sprintEndDate = "";
+        String daysLeft = "";
         JSONArray jsonArray = jBody.getJSONArray("issues");
         List<SprintTicket> ticketList = new LinkedList<>();
         for (int i = 0; i < jsonArray.length(); i++) {
+            if (i == 0) {
+                sprintEndDate = jsonArray.getJSONObject(i).getJSONObject("fields").getJSONArray("customfield_10000").get(0).toString();
+                daysLeft = getJiraDaysLeft(sprintEndDate);
+            }
             HashMap<String, String> ticketDetail = new HashMap<>();
             String keyValue = jsonArray.getJSONObject(i).getString("key");
             String summaryValue = jsonArray.getJSONObject(i).getJSONObject("fields").getString("summary");
@@ -95,20 +105,22 @@ public class JiraAPI {
             ticketList.add(tempTicket);
         }
         Collections.sort(ticketList);
-        String finalString = "*Welcome to ARGO*\n";
-        finalString += "*---------- Total: " + totalTickets + ", Done: " + doneTickets + " ----------*\n";
+        String finalString = ">*---------- Welcome to ARGO ---------- *\n";
+        finalString += ">* Total: " + totalTickets + ", Done: " + doneTickets + ", `" + daysLeft + "` to go." + " ---------- *\n";
         if (showAll == false) {
             for (SprintTicket ticket : ticketList) {
                 String symbol = getSymbol(ticket.getStatus());
-                finalString += "*" + ticket.getTicket() + "*   *\"" + ticket.getStatus() + "\"*   *" + ticket.getAssigneeName() + "*     " + symbol + "\n";
+                String ticketCode = ticket.getTicket();
+                finalString += "*" + createHyperlink(jiraTicketUrl + ticketCode, ticketCode) + "*   *\"" + ticket.getStatus() + "\"*   *" + ticket.getAssigneeName() + "*     " + symbol + "\n";
             }
         } else {
             for (SprintTicket ticket : ticketList) {
                 String symbol = getSymbol(ticket.getStatus());
-                finalString += "*" + ticket.getTicket() + "*   *\"" + ticket.getStatus() + "\"*   *" + ticket.getAssigneeName() + "*     " + symbol + "\n     " + ticket.getSummary() + "\n";
+                String ticketCode = ticket.getTicket();
+                finalString += "*" + createHyperlink(jiraTicketUrl + ticketCode, ticketCode) + "*   *\"" + ticket.getStatus() + "\"*   *" + ticket.getAssigneeName() + "*     " + symbol + "\n     " + ticket.getSummary() + "\n";
             }
         }
-        return finalString;
+        return finalString + getMessage(totalTickets, doneTickets);
     }
 
 
@@ -124,7 +136,7 @@ public class JiraAPI {
             symbol = ":ninja:";
         } else if (status.equalsIgnoreCase("In Review")) {
             symbol = ":time:";
-        }else if (status.equalsIgnoreCase("Analysis") || status.equalsIgnoreCase("Ready for Dev") || status.equalsIgnoreCase("To Do")) {
+        } else if (status.equalsIgnoreCase("Analysis") || status.equalsIgnoreCase("Ready for Dev") || status.equalsIgnoreCase("To Do")) {
             symbol = ":exclamation:";
         }
         return symbol;
@@ -163,5 +175,51 @@ public class JiraAPI {
         }
     }
 
+    private String createHyperlink(String url, String hyperlinkText) {
+        return "<" + url + "|" + hyperlinkText + ">";
+    }
+
+    private String getMessage(int total, int completed) {
+        int diff = total - completed;
+        if (diff == 0) {
+            return "`Congratulations!!! That was a clean sprint. :tada: :confetti_ball:`";
+        } else if (diff == 1) {
+            return "`One more push and we will get there! Come one team.`";
+        } else if (diff == 2) {
+            return "`We did a lot, lets just get these two tickets done and we will reach our goal.`";
+        } else if (diff == 3) {
+            return "`Guys, lets not slack any more, finish these three tickets.`";
+        } else {
+            return "";
+        }
+    }
+
+    private String getJiraDaysLeft(String jiraString) {
+        Date extractedDate = null;
+        try {
+            extractedDate = extractDate(jiraString, "(?<=API- )(.*)(?=,start)");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        long dateDiff = getDateDiff(new Date(), extractedDate, TimeUnit.DAYS);
+        return dateDiff + " days";
+    }
+
+    private Date extractDate(String text, String regex) throws ParseException {
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(text);
+        String date = "";
+        while (m.find()) {
+            date = m.group();
+        }
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date d = format.parse(date);
+        return d;
+    }
+
+    private static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
+        long diffInMillies = date2.getTime() - date1.getTime();
+        return timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS);
+    }
 
 }
